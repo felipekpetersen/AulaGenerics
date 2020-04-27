26c2b4343a48f9a18fef3eadf099e52466f86bea
Felipe Petersen <felipekpetersen@gmail.com>
2020-04-27 17:14:40 -0300

    exercicios resolvidos

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -18,6 +18,42 @@ pilha.push(intA)
 pilha.push(intB)
 
 //TODO: Fazer um método que recebe um Array e um elemento, retorna um Bool dizendo se o elemento se encontra no Array
+
+
+func contem<Elemento: Comparable>(elementos: [Elemento], elemento: Elemento) -> Bool {
+    elementos.contains(where: { (teste) -> Bool in
+        return teste == elemento
+    })}
+
+contem(elementos: [1,2,3,4,5,6], elemento: 3)
+
 //TODO: Extensão de Array contendo uma função que retorna a soma de todos os elementos, independente de qual o valor numérico
+
+extension Array where Element: Numeric{
+    
+    func sum() -> Element {
+        var sum = Element.zero
+        for i in self {
+            sum += i
+        }
+        return sum
+    }
+}
+
 //TODO: Fazer um método que recebe 2 Arrays e retorna um Int referente a quantidade de elementos presentes em ambos os Arrays
+
+func somaElementos<Element: Comparable>(arr1: [Element], arr2: [Element]) -> Int {
+    var contemEmAmbas = 0
+    for value in arr1 {
+        if arr2.contains(value) {
+            contemEmAmbas += 1
+        }
+    }
+    
+    return contemEmAmbas
+}
+
+somaElementos(arr1: [1,2,3,5], arr2: [1,3,4])
+
     //BONUS TODO: Fazer o método acima receber N arrays
+
